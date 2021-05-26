95c6e01fe38ff98a57c1e22a5cc95959a2d282a2
RingEric <77667005+RingEric@users.noreply.github.com>
2021-05-26 13:52:33 +0800

    flink入口

---
+++ src/main/scala/qm/entry/FlinkEntry.scala
@@ -0,0 +1,14 @@
+package qm.entry
+
+/**
+ * @ClassName: qm.entry
+ * @Description:
+ * @Author:LinYoung
+ * @Date: 2021/5/26
+ * @Time: 11:21
+ */
+object FlinkEntry {
+  def entry(): Unit = {
+
+  }
+}
