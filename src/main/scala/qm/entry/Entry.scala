8987d7f32f7c380ebe6ad9af9c5b3fa09c9f962b
RingEric <77667005+RingEric@users.noreply.github.com>
2021-05-26 13:52:11 +0800

    程序入口

---
+++ src/main/scala/qm/entry/Entry.scala
@@ -0,0 +1,14 @@
+package qm.entry
+
+/**
+ * @ClassName: qm.entry
+ * @Description: 程序处理入口
+ * @Author: LinYoung
+ * @Date: 2021/5/26
+ * @Time: 11:20
+ */
+object Entry {
+  def main(args: Array[String]): Unit = {
+    FlinkEntry.entry()
+  }
+}
